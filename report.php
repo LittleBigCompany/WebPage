15d75ffcc5b7d292c8af4fdd486e4b26b8e2d1f6
trolandus <johnnybe.beczcki@gmail.com>
2014-12-15 15:04:58 +0100

    Added mySQL service for our form - sends the data to database

--- report.php
+++ report.php
@@ -7,9 +7,18 @@
 		<?php
 			$firstname = $_POST['firstname'];
 			$problem = $_POST['problem'];
+			$mail = $_POST['mail'];
+			$from = 'johnnybe.beczcki@gmail.com';
+			$emailmsg = 'TEST';
 
 			echo 'First name: ' . $firstname . '<br />';
-			echo 'Problem: ' . $problem;
+			echo 'Problem: ' . $problem . '<br />';
+
+			$dbc = mysqli_connect('localhost', 'root', '', 'themine') or die('Error connecting to SQL database');
+
+			$query = "INSERT INTO problems (name, problem, mail) VALUES ('" . $firstname . "', '" . $problem . "', '" . $mail. "')";
+			$result = mysqli_query($dbc, $query) or die('Error querying database');
+			mysqli_close($dbc);
 		?>
 	</body>
 </html>
